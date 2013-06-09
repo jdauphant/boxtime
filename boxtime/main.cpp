2453057b372847a3210960b4462bdb35dbdff2be
Julien Dauphant <judauphant@gmail.com>
2013-06-09 12:08:20 +0200

    Add auto installation police

--- boxtime/main.cpp
+++ boxtime/main.cpp
@@ -3,10 +3,20 @@
 #include <QtCore>
 #include <QApplication>
 #include <QDesktopWidget>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication qApplication(argc, argv);
+    if(-1 == QFontDatabase::addApplicationFont("://ressources/Nexa Light.otf"))
+    {
+        qDebug() << "Impossible to install font Nexa Light.otf";
+    }
+    if(-1 == QFontDatabase::addApplicationFont("://ressources/Nexa Bold.otf"))
+    {
+        qDebug() << "Impossible to install font Nexa Bold.otf";
+    }
+
     TaskWidget taskWidget;
 
     TaskController * taskController = TaskController::getInstance();
